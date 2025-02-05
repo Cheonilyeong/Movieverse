51237562a4296dd995420c26820ed2b135fe2c36
천일영 <147039081+Cheonilyeong@users.noreply.github.com>
2025-02-05 20:32:14 +0900

    #15 배너 UI 구현

--- app/src/main/java/com/ilyeong/movieverse/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/ilyeong/movieverse/presentation/home/HomeFragment.kt
@@ -1,6 +1,8 @@
 package com.ilyeong.movieverse.presentation.home
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import com.ilyeong.movieverse.databinding.FragmentHomeBinding
 import com.ilyeong.movieverse.presentation.common.BaseFragment
@@ -9,4 +11,11 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
     override val viewBindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentHomeBinding
         get() = FragmentHomeBinding::inflate
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.vpBanner.offscreenPageLimit = 1
+        binding.vpBanner.adapter = BannerAdapter()
+    }
 }
