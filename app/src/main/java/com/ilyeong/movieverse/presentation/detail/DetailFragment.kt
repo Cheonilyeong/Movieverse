d309b489a10c9c5b83739bb505e64d679bb407dd
천일영 <147039081+Cheonilyeong@users.noreply.github.com>
2025-04-01 17:19:43 +0900

    #49 Detail Shimmer 적용

--- app/src/main/java/com/ilyeong/movieverse/presentation/detail/DetailFragment.kt
+++ app/src/main/java/com/ilyeong/movieverse/presentation/detail/DetailFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -72,10 +73,16 @@ class DetailFragment : BaseFragment<FragmentDetailBinding>() {
             viewModel.uiState.collect {
                 when (it) {
                     DetailUiState.Loading -> {
-                        // todo
+                        binding.sfl.startShimmer()
+                        binding.sfl.isVisible = true
+                        binding.content.isVisible = false
                     }
 
                     is DetailUiState.Success -> {
+                        binding.sfl.stopShimmer()
+                        binding.sfl.isVisible = false
+                        binding.content.isVisible = true
+
                         val movie = it.movie
 
                         binding.ivBackdrop.load(movie.backdropPath) {
