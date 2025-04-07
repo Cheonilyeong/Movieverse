1f69b5c8c28a46a53743c44ea8e6af34a22cfc37
천일영 <147039081+Cheonilyeong@users.noreply.github.com>
2025-04-07 18:39:55 +0900

    #51 불필요 어노테이션 삭제

--- app/src/main/java/com/ilyeong/movieverse/presentation/detail/DetailViewModel.kt
+++ app/src/main/java/com/ilyeong/movieverse/presentation/detail/DetailViewModel.kt
@@ -11,7 +11,6 @@ import com.ilyeong.movieverse.presentation.detail.model.DetailEvent
 import com.ilyeong.movieverse.presentation.detail.model.DetailEvent.ShowMessage
 import com.ilyeong.movieverse.presentation.detail.model.DetailUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -37,7 +36,6 @@ class DetailViewModel @Inject constructor(
     private val _events = MutableSharedFlow<DetailEvent>()
     val events = _events.asSharedFlow()
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     fun loadData(movieId: Int) {
         val detailFlow = movieRepository.getMovieDetail(movieId)
         val creditFlow = movieRepository.getMovieCredit(movieId)
