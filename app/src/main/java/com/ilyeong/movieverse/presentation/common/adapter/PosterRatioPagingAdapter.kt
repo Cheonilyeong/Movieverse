3aa4776ffcc1f922043a9ea51928419896861bfd
천일영 <147039081+Cheonilyeong@users.noreply.github.com>
2025-04-10 19:50:23 +0900

    #55 GenreMoviePageData Ui에 그리기

--- app/src/main/java/com/ilyeong/movieverse/presentation/common/adapter/PosterRatioPagingAdapter.kt
+++ app/src/main/java/com/ilyeong/movieverse/presentation/common/adapter/PosterRatioPagingAdapter.kt
@@ -1,21 +1,25 @@
 package com.ilyeong.movieverse.presentation.common.adapter
 
 import android.view.ViewGroup
-import androidx.recyclerview.widget.ListAdapter
+import androidx.paging.PagingDataAdapter
 import com.ilyeong.movieverse.domain.model.Movie
 import com.ilyeong.movieverse.presentation.common.viewholder.PosterRatioViewHolder
 import com.ilyeong.movieverse.presentation.util.ItemClickListener
 import com.ilyeong.movieverse.presentation.util.MovieDiffUtil
 
-class PosterRatioAdapter(
+class PosterRatioPagingAdapter(
     private val itemClickListener: ItemClickListener
-) : ListAdapter<Movie, PosterRatioViewHolder>(MovieDiffUtil) {
+) : PagingDataAdapter<Movie, PosterRatioViewHolder>(MovieDiffUtil) {
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ) = PosterRatioViewHolder.create(parent)
 
-    override fun onBindViewHolder(holder: PosterRatioViewHolder, position: Int) {
-        holder.bind(getItem(position), itemClickListener)
+    override fun onBindViewHolder(
+        holder: PosterRatioViewHolder,
+        position: Int
+    ) {
+        val item = getItem(position) ?: return
+        holder.bind(item, itemClickListener)
     }
 }
--- app/src/main/java/com/ilyeong/movieverse/presentation/genre/GenreViewModel.kt
+++ app/src/main/java/com/ilyeong/movieverse/presentation/genre/GenreViewModel.kt
@@ -2,20 +2,19 @@ package com.ilyeong.movieverse.presentation.genre
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import com.ilyeong.movieverse.data.repository.MovieRepository
-import com.ilyeong.movieverse.presentation.genre.model.GenreEvent
-import com.ilyeong.movieverse.presentation.genre.model.GenreEvent.ShowMessage
 import com.ilyeong.movieverse.presentation.genre.model.GenreUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import javax.inject.Inject
 
@@ -24,34 +23,30 @@ class GenreViewModel @Inject constructor(
     private val movieRepository: MovieRepository,
 ) : ViewModel() {
 
-    private val _uiState = MutableStateFlow<GenreUiState>(GenreUiState.Loading)
-    val uiState: StateFlow<GenreUiState> = _uiState.asStateFlow()
+    private val _genreId = MutableStateFlow<Int?>(null)
 
-    private val _events = MutableSharedFlow<GenreEvent>()
-    val events = _events.asSharedFlow()
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val genreMoviePaging = _genreId
+        .filterNotNull()
+        .flatMapLatest {
+            movieRepository.getMovieListByGenrePaging(it)
+        }
+        .cachedIn(viewModelScope)
 
-    fun loadData(genreId: Int) {
-        val movieList = movieRepository.getMovieListByGenre(genreId)
-        val genreList = movieRepository.getMovieGenreList()
+    private val _uiState = MutableStateFlow<GenreUiState>(GenreUiState(null))
+    val uiState = _uiState.asStateFlow()
 
-        combine(movieList, genreList) { movieList, genreList ->
-            _uiState.value = GenreUiState.Success(
-                genre = genreList.first { it.id == genreId },
-                movieList = movieList
-            )
-        }.onStart {
-            when (_uiState.value) {
-                is GenreUiState.Loading -> {}       // no-op
-                is GenreUiState.Success -> {}       // no-op
-                is GenreUiState.Failure -> _uiState.value = GenreUiState.Loading
-            }
-            delay(1000L)     // Shimmer Test
-        }.catch {
-            when (_uiState.value) {
-                is GenreUiState.Loading -> _uiState.value = GenreUiState.Failure
-                is GenreUiState.Success -> _events.emit(ShowMessage(it))
-                is GenreUiState.Failure -> _events.emit(ShowMessage(it))
-            }
-        }.launchIn(viewModelScope)
+    fun setGenreId(genreId: Int) {
+        _genreId.value = genreId
+
+        movieRepository.getMovieGenreList()
+            .onStart {
+                _uiState.value = GenreUiState(null)
+                delay(1000L)     // Shimmer Test
+            }.onEach { genreList ->
+                _uiState.value = GenreUiState(genreList.find { it.id == genreId })
+            }.catch {
+                _uiState.value = GenreUiState(null)
+            }.launchIn(viewModelScope)
     }
 }
