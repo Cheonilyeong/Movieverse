33d8740a77be0d944a054114724244014b616303
천일영 <147039081+Cheonilyeong@users.noreply.github.com>
2025-04-10 19:48:41 +0900

    #55 MovieResponse default 값 지정

--- app/src/main/java/com/ilyeong/movieverse/data/model/MovieResponse.kt
+++ app/src/main/java/com/ilyeong/movieverse/data/model/MovieResponse.kt
@@ -7,20 +7,20 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class MovieResponse(
-    @SerialName("adult") val adult: Boolean,
+    @SerialName("adult") val adult: Boolean = false,
     @SerialName("backdrop_path") val backdropPath: String = "",
     @SerialName("genre_ids") val genreIdList: List<Int> = emptyList(),
     @SerialName("id") val id: Int,
-    @SerialName("original_language") val originalLanguage: String,
-    @SerialName("original_title") val originalTitle: String,
-    @SerialName("overview") val overview: String,
-    @SerialName("popularity") val popularity: Double,
+    @SerialName("original_language") val originalLanguage: String = "",
+    @SerialName("original_title") val originalTitle: String = "",
+    @SerialName("overview") val overview: String = "",
+    @SerialName("popularity") val popularity: Double = 0.0,
     @SerialName("poster_path") val posterPath: String = "",
-    @SerialName("release_date") val releaseDate: String,
-    @SerialName("title") val title: String,
-    @SerialName("video") val video: Boolean,
-    @SerialName("vote_average") val voteAverage: Double,
-    @SerialName("vote_count") val voteCount: Int
+    @SerialName("release_date") val releaseDate: String = "",
+    @SerialName("title") val title: String = "",
+    @SerialName("video") val video: Boolean = false,
+    @SerialName("vote_average") val voteAverage: Double = 0.0,
+    @SerialName("vote_count") val voteCount: Int = 0
 )
 
 fun MovieResponse.toDomain() = Movie(
