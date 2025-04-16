8c39c01f5164887192e862f111b2df24c62ef490
천일영 <147039081+Cheonilyeong@users.noreply.github.com>
2025-04-16 20:53:30 +0900

    #67 불필요 log 제거

--- app/src/main/java/com/ilyeong/movieverse/presentation/watchlist/WatchlistViewModel.kt
+++ app/src/main/java/com/ilyeong/movieverse/presentation/watchlist/WatchlistViewModel.kt
@@ -1,12 +1,10 @@
 package com.ilyeong.movieverse.presentation.watchlist
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
 import com.ilyeong.movieverse.data.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,9 +13,4 @@ class WatchlistViewModel @Inject constructor(
 ) : ViewModel() {
 
     val watchlistPaging = userRepository.getWatchlistMoviePaging().cachedIn(viewModelScope)
-        .onEach { Log.d("watchlistfragment", "New paging data emitted ${it}") }
-
-    init {
-        Log.d("WatchlistFragment", "viewmodel init")
-    }
 }
