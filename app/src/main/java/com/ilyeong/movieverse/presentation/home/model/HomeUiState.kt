08c2a54a07b22ab399274b675499baffc45772e3
천일영 <147039081+Cheonilyeong@users.noreply.github.com>
2025-04-16 20:21:01 +0900

    #67 HomeUiState 수정

--- app/src/main/java/com/ilyeong/movieverse/presentation/home/model/HomeUiState.kt
+++ app/src/main/java/com/ilyeong/movieverse/presentation/home/model/HomeUiState.kt
@@ -9,12 +9,6 @@ sealed interface HomeUiState {
     data class Success(
         val bannerMovieList: List<Movie>,
         val genreList: List<Genre>,
-        val topRatedMovieList: List<Movie>,
-        val upcomingMovieList: List<Movie>,
-        val popularMovieList: List<Movie>,
-        val nowPlayingMovieList: List<Movie>,
-        val trendingWeekMovieList: List<Movie>,
-        val watchlistMovieList: List<Movie>,
     ) : HomeUiState
 
     data object Failure : HomeUiState
