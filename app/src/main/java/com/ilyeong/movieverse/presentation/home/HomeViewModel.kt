78af6f68a9f334b3da89f0916ee71f72cd667b0b
천일영 <147039081+Cheonilyeong@users.noreply.github.com>
2025-04-16 20:34:32 +0900

    #67 Home화면 Api 데이터 그리기

--- app/src/main/java/com/ilyeong/movieverse/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/ilyeong/movieverse/presentation/home/HomeViewModel.kt
@@ -2,91 +2,61 @@ package com.ilyeong.movieverse.presentation.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import com.ilyeong.movieverse.data.repository.MovieRepository
 import com.ilyeong.movieverse.data.repository.UserRepository
-import com.ilyeong.movieverse.domain.model.Genre
-import com.ilyeong.movieverse.domain.model.Movie
 import com.ilyeong.movieverse.domain.model.TimeWindow
-import com.ilyeong.movieverse.presentation.home.model.HomeEvent
-import com.ilyeong.movieverse.presentation.home.model.HomeEvent.ShowMessage
 import com.ilyeong.movieverse.presentation.home.model.HomeUiState
+import com.ilyeong.movieverse.presentation.home.model.HomeUiState.Failure
+import com.ilyeong.movieverse.presentation.home.model.HomeUiState.Loading
+import com.ilyeong.movieverse.presentation.home.model.HomeUiState.Success
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val movieRepository: MovieRepository,
-    private val userRepository: UserRepository,
+    userRepository: UserRepository,
 ) : ViewModel() {
 
-    private val _uiState = MutableStateFlow<HomeUiState>(HomeUiState.Loading)
+    private val _uiState = MutableStateFlow<HomeUiState>(Loading)
     val uiState: StateFlow<HomeUiState> = _uiState.asStateFlow()
 
-    private val _events = MutableSharedFlow<HomeEvent>()
-    val events = _events.asSharedFlow()
+    val watchlistPaging = userRepository.getWatchlistMoviePaging().cachedIn(viewModelScope)
+    val upcomingMoviePaging =
+        movieRepository.getUpcomingMoviePaging(maxPage = 3).cachedIn(viewModelScope)
+    val popularMoviePaging =
+        movieRepository.getPopularMoviePaging(maxPage = 3).cachedIn(viewModelScope)
+    val nowPlayingMoviePaging =
+        movieRepository.getNowPlayingMoviePaging(maxPage = 3).cachedIn(viewModelScope)
+    val trendingWeekMoviePaging =
+        movieRepository.getTrendingMoviePaging(timeWindow = TimeWindow.WEEK, maxPage = 3)
+            .cachedIn(viewModelScope)
+    val topRatedMoviePaging =
+        movieRepository.getTopRatedMoviePaging(maxPage = 3).cachedIn(viewModelScope)
 
     fun loadData() {
+        if (_uiState.value is Success) return
+
         val trendingDayFlow = movieRepository.getTrendingMovieList(TimeWindow.DAY)
         val genreFlow = movieRepository.getMovieGenreList()
-        val topRatedFlow = movieRepository.getTopRatedMovieList()
-        val upcomingFlow = movieRepository.getUpcomingMovieList()
-        val popularFlow = movieRepository.getPopularMovieList()
-        val nowPlayingFlow = movieRepository.getNowPlayingMovieList()
-        val trendingWeekFlow = movieRepository.getTrendingMovieList(TimeWindow.WEEK)
-        val watchlistFlow = userRepository.getWatchlist(1)
 
-        combine(
-            trendingDayFlow,
-            genreFlow,
-            topRatedFlow,
-            upcomingFlow,
-            popularFlow,
-            nowPlayingFlow,
-            trendingWeekFlow,
-            watchlistFlow
-        ) { dataList ->
-            val bannerMovieList = dataList[0] as List<Movie>
-            val genreList = dataList[1] as List<Genre>
-            val topRatedMovieList = dataList[2] as List<Movie>
-            val upcomingMovieList = dataList[3] as List<Movie>
-            val popularMovieList = dataList[4] as List<Movie>
-            val nowPlayingMovieList = dataList[5] as List<Movie>
-            val trendingWeekMovieList = dataList[6] as List<Movie>
-            val watchlistMovieList = dataList[7] as List<Movie>
-
-            _uiState.value = HomeUiState.Success(
-                bannerMovieList = bannerMovieList.take(5),
-                genreList = genreList,
-                topRatedMovieList = topRatedMovieList,
-                upcomingMovieList = upcomingMovieList,
-                popularMovieList = popularMovieList,
-                nowPlayingMovieList = nowPlayingMovieList,
-                trendingWeekMovieList = trendingWeekMovieList,
-                watchlistMovieList = watchlistMovieList,
-            )
-        }.onStart {
-            when (_uiState.value) {
-                is HomeUiState.Loading -> {}    // no-op
-                is HomeUiState.Success -> {}    // no-op
-                is HomeUiState.Failure -> _uiState.value = HomeUiState.Loading
+        combine(trendingDayFlow, genreFlow, ::Pair)
+            .onStart {
+                _uiState.value = Loading
+                delay(2000L)
             }
-            delay(1000L)    // Shimmer Tess
-        }.catch { error ->
-            when (_uiState.value) {
-                is HomeUiState.Loading -> _uiState.value = HomeUiState.Failure
-                is HomeUiState.Success -> _events.emit(ShowMessage(error))
-                is HomeUiState.Failure -> _events.emit(ShowMessage(error))
-            }
-        }.launchIn(viewModelScope)
+            .onEach { _uiState.value = Success(it.first, it.second) }
+            .catch { _uiState.value = Failure }
+            .launchIn(viewModelScope)
     }
 }
